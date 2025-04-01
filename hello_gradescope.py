3c24cf320019ef59077dab238c84b34e4f8f0f5b
Eduardo Perez <eduardo.perez.student@gmail.com>
2025-04-01 10:26:17 -0700

    Initial

--- hello_gradescope.py
+++ hello_gradescope.py
@@ -1,9 +1,9 @@
-# Name:
-# OSU Email:
+# Name: Eduardo Perez
+# OSU Email: pereze4@oregonstate.edu
 # Course: CS261 - Data Structures
-# Assignment:
-# Due Date:
-# Description:
+# Assignment 0: GradeScope Submission
+# Due Date: April 9, 2025
+# Description: This submission is simply to test if gradescope is properly working
 
 
 def gradescope_test():
